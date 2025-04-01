1e256e30a9a1931dfa106d7ed0c21bd6a94d3c2c
Sofia Katzman <katzmansof@gmail.com>
2025-04-01 14:10:29 -0400

    add seed to app startup

--- server/app.py
+++ server/app.py
@@ -1,18 +1,24 @@
 from flask import render_template
 from server.config import app, jsonify
 from server.routes.routes import *
+from server.seed import seed
+from server.models.models import User
 
 # system logging
 import sys
 print(sys.path)
 
-# Import your other modules here (models, routes, etc.)
+def check_and_seed():
+    with app.app_context():
+        if User.query.first() is None:
+            print("No users found, seeding database...")
+            seed()
+        else:
+            print("Users already exist. Skipping seeding.")
 
-# Routes to render the "index.html" template
 
 @app.route('/api/users')
 def api_users():
-    # Add logic here to handle API requests if needed
     return jsonify({"message": "API endpoint for users"})
 
 @app.route('/')
@@ -23,4 +29,5 @@ def index(id=0):
     return render_template("index.html")
 if __name__ == '__main__':
     # socketio.run(app, host='0.0.0.0', port=8000, debug=False)
+    check_and_seed()
     app.run(port=5555, debug=True)
